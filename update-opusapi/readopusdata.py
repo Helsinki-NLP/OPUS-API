0fe9e720c54fbd81bf20e8996a769493a1c077fd
miau1 <aulamo.mikko@gmail.com>
2021-08-12 12:55:05 +0300

    new database updating script

--- update-opusapi/readopusdata.py
+++ update-opusapi/readopusdata.py
@@ -1,134 +1,66 @@
 import re
+import urllib.request
 import sqlitetest as st
 
+URL_BASE = "https://object.pouta.csc.fi/OPUS-index"
+
+def read_url(url):
+    return urllib.request.urlopen(url).read().decode("utf-8").split("\n")
+
+def get_languages(languages_raw):
+    if languages_raw == "README":
+        return "", ""
+    m = re.search("^(.*?)(\.|$)", languages_raw)
+    if m:
+        languages = m.group(1).split("-")
+    if len(languages) == 2:
+        return languages[0], languages[1]
+    else:
+        return languages[0], ""
+
 st.main()
-
-f = open("opusdata.txt")
-
-errorlog = open("opusdata_errors.log", "w")
-errornum = 0
-
-url = ""
 
 conn = st.create_connection("opusdata.db")
 cur = conn.cursor()
 
-latest = ""
+corpora = read_url("{}/{}".format(URL_BASE, "index.txt"))
 
-oid=0
-for line in f:
-    '''
-for i in range(500):
-    line = f.readline()
-    #print(line)
-    #'''
-    oid += 1
+prev_corpus_name = ""
 
-    origLine = line
-    line = line.strip()
-    line = line.split(" ")
-    if line[0] == "" or line[0] == "total":
-#        print("")
-        continue
-    if line[0][-1] == ":":
-        url = line[0][:-1]+"/"
-
-    if len(line) >= 2 and line[0][-1] != ":" and line[-1][-1] != "/":
-        #print(url, end=" ")
-        if " latest -> " in origLine:
-            latest = line[-1]
-
-        m = re.search("/proj/nlpl/data/OPUS/(.*?)/(.*?)/(.*?)/(.*?)$", url+line[-1])
+for line in corpora:
+    m = re.search("(.*?)/", line)
+    if m:
+        corpus_name = m.group(1)
+    if corpus_name != prev_corpus_name:
+        print("Processing corpus {}".format(corpus_name))
+        prev_corpus_name = corpus_name
+        releases = read_url("{}/{}/{}".format(URL_BASE, corpus_name, "releases.txt"))
+        m = re.search("(.*?)\t", releases[-2])
         if m:
-            corpus = m.group(1)
-            version = m.group(2)
-            preprocessing = m.group(3)
-            if preprocessing == "info":
-                continue
-            rest = m.group(4)
-            if "Makefile" in rest:
-                continue
-            #print(corpus, version, preprocessing, rest)
-                
-            if version == latest:
-                isLatest = "True"
-            else:
-                isLatest = "False"
+            latest = m.group(1)
+            m = re.search("(.*?)\t", releases[-2])
+        index = read_url("{}/{}/{}".format(URL_BASE, corpus_name, "index.txt"))
+        for item in index:
+            m = re.search("^(.*?)/(.*?)/(.*?)(/|$)", item)
+            if m:
+                languages_raw = m.group(3)
+                source, target = get_languages(languages_raw)
+                corpus = corpus_name
+                preprocessing = m.group(2)
+                version = m.group(1)
+                url = "https://object.pouta.csc.fi/OPUS-{}/{}".format(corpus_name, item)
+                size = ""
+                documents = ""
+                alignment_pairs = ""
+                source_tokens = ""
+                target_tokens = ""
+                isLatest = version == latest
 
-            sourceandtarget = False
+            opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, isLatest)
 
-            altstr = ""
-            if ".alt.xml.gz" in rest:
-                altstr = ".alt"
-
-            if rest.startswith(corpus):
-                n = re.search("\.(.*?)\.gz", rest)
-                if n:
-                    source = n.group(1).replace("raw.", "")
-                    target = ""
-                    infopath = "/proj/nlpl/data/OPUS/{0}/{1}/info/{2}{3}.info".format(corpus, version, source, altstr)
-            elif "-" not in rest:
-                n = re.search("^(.*?)\.", rest)
-                if n:
-                    source = n.group(1)
-                    target = ""
-                    infopath = "/proj/nlpl/data/OPUS/{0}/{1}/info/{2}{3}.info".format(corpus, version, source, altstr)
-            else:
-                n = re.search("(.*?)\-(.*?)[\.|/]", rest)
-                if n:
-                    source = n.group(1)
-                    target = n.group(2)
-                    infopath = "/proj/nlpl/data/OPUS/{0}/{1}/info/{2}-{3}{4}.info".format(corpus, version, source, target, altstr)
-                    sourceandtarget = True
-
-            if source in ["", "*", "logfiles"]:
-                continue
-
-            numbers = ["","","",""]
-
-            try:
-                with open(infopath) as infof:
-                    numbers = infof.readlines()
-            except Exception as e:
-                errorlog.write(str(e) + "\n")
-                errornum += 1
-
-            #print(corpus, version, preprocessing, source, target)
-            link = url+line[-1]
-            link = link.replace("/proj/nlpl/data/OPUS/", "")
-
-
-            #print(opusfile)
-
-            if sourceandtarget:
-                if len(numbers) < 4:
-                    #print(link)
-                    numbers = ["","","",""]
-                opusfile = (source, target, corpus, preprocessing, version, link, line[0], numbers[0].strip(), numbers[1].strip(), numbers[2].strip(), numbers[3].strip(), isLatest)
-                sql = '''INSERT INTO opusfile(source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'''
-            else:
-                opusfile = (source, target, corpus, preprocessing, version, link, line[0], numbers[0].strip(), numbers[1].strip(), numbers[2].strip(), isLatest)
-                sql = '''INSERT INTO opusfile(source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, latest) VALUES(?,?,?,?,?,?,?,?,?,?,?)'''
-                
+            sql = '''INSERT INTO opusfile(source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'''
 
             cur.execute(sql, opusfile)
-                
-        else:
-            pass
-            #print("")
 
-    corpus = ""
-    version = ""
-    preprocessing = ""
-    source = ""
-    target = ""
-
-    if oid % 100 == 0:
-        print(oid, "lines processed,", errornum, "errors encountered", end="\r")
-
-print(oid, "lines processed,", errornum, "errors encountered", end="\r")
-print("\nopusdata.db created! Check 'opusdata_error.log' for errors.\n")
-errorlog.close()
-f.close()
 conn.commit()
 conn.close()
--- update-opusapi/update_opus_database.sh
+++ update-opusapi/update_opus_database.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-module load python-env/3.5.3
+bu_name=backups/$(date +"%Y%m%d%H%M%S")_opusdata.db
 
-echo -e "\nReading file structure from /proj/nlpl/data/OPUS/ ..."
+echo -e "\nCreating backup db to $bu_name"
 
-ls -Rpsl /proj/nlpl/data/OPUS > opusdata.txt
-
-echo `grep -c ^ opusdata.txt` "lines read"
+mv opusdata.db $bu_name
 
 echo -e "\nCreating opusdata.db ..."
-
-mv opusdata.db backups/$(date +"%Y%m%d%H%M%S")_opusdata.db
 
 python3 readopusdata.py
 
 echo -e "\nopusdata.db created"
 
-#scp -i ~/.ssh/pouta opusdata.db cloud-user@vm1617.kaj.pouta.csc.fi:~/opusdata-new.db
-#
-#ssh -i ~/.ssh/pouta cloud-user@vm1617.kaj.pouta.csc.fi \
-#"sudo mv /var/www/opusdata.db /var/www/opusdata_backup/$(date +"%Y%m%d%H%M%S")_opusdata.db
-#sudo mv ~/opusdata-new.db /var/www/opusdata.db
-#sudo chmod 774 /var/www/opusdata.db
-#sudo systemctl restart apache2
-#exit"
-#
-#echo -e "\nOPUS-API database updated on vm1617"
-#

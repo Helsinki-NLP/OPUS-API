077e15fa4cd20545bf5f2c07ed14cb791d4a7eaf
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2021-08-15 01:25:38 +0300

    opustools expect size in kB, does it?

--- update-opusapi/readopusdata.py
+++ update-opusapi/readopusdata.py
@@ -92,7 +92,8 @@ for line in corpora:
                     version = m2.group(1)
                     # url = "https://object.pouta.csc.fi/OPUS-{}/{}".format(corpus_name, item_path)
                     url = "{}/{}".format(corpus_name, item_path)
-                    size = m.group(1)
+                    # size = m.group(1)
+                    size = int(int(m.group(1))/1000)
                     documents, alignment_pairs, source_tokens, target_tokens = get_item_info(source, target, preprocessing, info_dict, corpus_name)
                     if version == latest:
                         isLatest = 'True'

9182a435a60e9655f6a2710a40e0a57d02ca4f2f
miau1 <aulamo.mikko@gmail.com>
2022-11-24 15:58:45 +0200

    find errors

--- update-opusapi/test.py
+++ update-opusapi/test.py
@@ -34,106 +34,105 @@ def execute_sql(cur, opusfile):
     sql = '''INSERT INTO opusfile(source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'''
     cur.execute(sql, opusfile)
 
-def get_lang_info(name, data, data_type):
+def get_lang_info(name, data, data_type, info):
     source, target, documents, alignment_pairs, source_tokens, target_tokens = '', '', '', '', '', ''
     source = name
     if data_type == 'bitexts':
         names = name.split('-')
         if len(names) != 2:
-            logging.warning(f'{data_type}: cannot split name "{name}" into two language codes')
+            logging.warning(f'{info} {data_type} {name}: cannot split name "{name}" into two language codes')
         else:
             source, target = names
     documents = ''
     if data_type in ['bitexts', 'monolingual']:
-        data['files']
         documents = data.get('files', '')
         if documents == '':
-            logging.warning(f'{data_type} is missing "files"')
+            logging.warning(f'{info} {data_type} {name} is missing "files"')
     if data_type in ['bitexts', 'moses']:
         alignment_pairs = data.get('alignments', '')
         if alignment_pairs == '':
-            logging.warning(f'{data_type} is missing "alignments"')
+            logging.warning(f'{info} {data_type} {name} is missing "alignments"')
     elif data_type == 'tmx':
         alignment_pairs = data.get('translation units', '')
         if alignment_pairs == '':
-            logging.warning(f'{data_type} is missing "translation units"')
+            logging.warning(f'{info} {data_type} {name} is missing "translation units"')
     elif data_type == 'monolingual':
         alignment_pairs = data.get('sentences', '')
         if alignment_pairs == '':
-            logging.warning(f'{data_type} is missing "sentences"')
+            logging.warning(f'{info} {data_type} {name} is missing "sentences"')
     if data_type == 'monolingual':
         source_tokens = data.get('tokens', '')
         if source_tokens == '':
-            logging.warning(f'{data_type} is missing "tokens"')
+            logging.warning(f'{info} {data_type} {name} is missing "tokens"')
         target_tokens = ''
     else:
-        source_tokens = data.get('source language tokens')
+        source_tokens = data.get('source language tokens', '')
         if source_tokens == '':
-            logging.warning(f'{data_type} is missing "source language tokens"')
-        target_tokens = data.get('target language tokens')
+            logging.warning(f'{info} {data_type} {name} is missing "source language tokens"')
+        target_tokens = data.get('target language tokens', '')
         if target_tokens == '':
-            logging.warning(f'{data_type} is missing "target language tokens"')
+            logging.warning(f'{info} {data_type} {name} is missing "target language tokens"')
 
     return source, target, documents, alignment_pairs, source_tokens, target_tokens
 
-def get_size_url_prep(data, data_type):
+def get_size_url_prep(name, data, data_type, info):
     size, url, preprocessing = '','',''
     if data_type in ['tmx', 'moses']:
         size = data.get('download size', '')
         if size == '':
-            logging.warning(f'{data_type} is missing "download size"')
+            logging.warning(f'{info} {data_type} {name} is missing "download size"')
         else:
             size = int(int(size)/1024)
         url = data.get('download url', '')
         if url == '':
-            logging.warning(f'{data_type} is missing "download url"')
+            logging.warning(f'{info} {data_type} {name} is missing "download url"')
     elif data_type in ['bitexts', 'monolingual']:
         size = data.get('size', '')
         if size == '':
-            logging.warning(f'{data_type} is missing "size"')
+            logging.warning(f'{info} {data_type} {name} is missing "size"')
         else:
             size = int(int(size)/1024)
-        url = data.get('url')
+        url = data.get('url', '')
         if url == '':
-            logging.warning(f'{data_type} is missing "url"')
+            logging.warning(f'{info} {data_type} {name} is missing "url"')
 
     pre_step = url.split('/')
     if len(pre_step) < 2:
-        logging.warning(f'{data_type}: cannot find preprocessing from {url}')
+        logging.warning(f'{info} {data_type} {name}: cannot find preprocessing from url "{url}"')
     else:
         preprocessing = pre_step[-2]
 
     return size, url, preprocessing
 
-def get_tmx_entries(corpus, version, latest, tmx, cur):
+def get_tmx_entries(corpus, version, latest, tmx, cur, info):
     for item in tmx:
-        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, tmx[item], 'tmx')
-        size, url, preprocessing = get_size_url_prep(tmx[item], 'tmx')
+        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, tmx[item], 'tmx', info)
+        size, url, preprocessing = get_size_url_prep(item, tmx[item], 'tmx', info)
         opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
         execute_sql(cur, opusfile)
 
-def get_moses_entries(corpus, version, latest, moses, cur):
+def get_moses_entries(corpus, version, latest, moses, cur, info):
     for item in moses:
-        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, moses[item], 'moses')
-        size, url, preprocessing = get_size_url_prep(moses[item], 'moses')
+        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, moses[item], 'moses', info)
+        size, url, preprocessing = get_size_url_prep(item, moses[item], 'moses', info)
         opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
         execute_sql(cur, opusfile)
 
-def get_monolingual_entries(corpus, version, latest, monolingual, cur):
+def get_monolingual_entries(corpus, version, latest, monolingual, cur, info):
     for item in monolingual:
-        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, monolingual[item], 'monolingual')
+        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, monolingual[item], 'monolingual', info)
         for entry in monolingual[item]['downloads'].items():
-            size, url, preprocessing = get_size_url_prep(entry[1], 'monolingual')
+            size, url, preprocessing = get_size_url_prep(item, entry[1], 'monolingual', info)
             opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
             execute_sql(cur, opusfile)
 
-def get_bitext_entries(corpus, version, latest, bitexts, cur):
+def get_bitext_entries(corpus, version, latest, bitexts, cur, info):
     for item in bitexts:
-        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, bitexts[item], 'bitexts')
+        source, target, documents, alignment_pairs, source_tokens, target_tokens = get_lang_info(item, bitexts[item], 'bitexts', info)
         for entry in bitexts[item]['downloads'].items():
             # exclude monolingual files, they are added in the monolingual phase
             if 'language' not in entry[0]:
-                size, url, preprocessing = get_size_url_prep(entry[1], 'bitexts')
+                size, url, preprocessing = get_size_url_prep(item, entry[1], 'bitexts', info)
                 opusfile = (source, target, corpus, preprocessing, version, url, size, documents, alignment_pairs, source_tokens, target_tokens, latest)
                 execute_sql(cur, opusfile)
 
@@ -168,7 +167,7 @@ def main():
                 latest = 'False'
                 if version == latest_v:
                     latest = 'True'
-                stats = info.replace('info', 'statistics')
+                stats = info.replace('info.yaml', 'statistics.yaml')
                 try:
                     corpus_data = read_url_yaml(URL_BASE + stats)
                 except (yaml.reader.ReaderError, urllib.error.HTTPError) as e:
@@ -179,13 +178,17 @@ def main():
                                 'monolingual': get_monolingual_entries,
                                 'moses': get_moses_entries,
                                 'tmx': get_tmx_entries}
+                
+                if not corpus_data:
+                    logging.error(f'{info}, corpus_data is empty')
+                    continue
 
                 for item in get_entries.keys():
                     sub_data = corpus_data.get(item)
-                if sub_data:
-                    get_entries[item](corpus, version, latest, sub_data, cur)
-                else:
-                    logging.warning(f'{info}, {item} data missing')
+                    if sub_data:
+                        get_entries[item](corpus, version, latest, sub_data, cur, info)
+                    else:
+                        logging.warning(f'{info}, {item} data missing')
 
     con.commit()
     con.close()

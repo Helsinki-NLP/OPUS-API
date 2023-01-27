848c3d1fa4b6db904c43886da448e0fb981d8439
miau1 <aulamo.mikko@gmail.com>
2023-01-27 16:55:18 +0200

    refactoring

--- opusapi.py
+++ opusapi.py
@@ -1,173 +1,113 @@
-from flask import Flask, render_template, request, jsonify
-from sqlalchemy import create_engine
 import os
+import sqlite3
+
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-opusapi_connection = create_engine('sqlite:///'+os.environ["OPUSAPI_DB"])
-    
-def make_sql_command(parameters, direction):
-    sql_command = "SELECT * FROM opusfile WHERE "
-    
-    so = parameters[0][1]
-    ta = parameters[1][1]
-
-    ret_param = []
-    
-    if direction:
-        sql_command += "((source='"+so+"' AND target='"+ta+"') OR (source='"+so+\
-                       "' AND target='') OR (source='"+ta+"' AND target='')) AND "
-        parameters[0] = ("source", "#EMPTY#")
-        parameters[1] = ("target", "#EMPTY#")
-    
-    if ta == "#EMPTY#" and so != "#EMPTY#":
-        sql_command += "((source='"+so+"') or (target='"+so+"')) AND "
-        parameters[0] = ("source", "#EMPTY#")
-     
-    for i in parameters:
-        if i[1] != "#EMPTY#":
-            sql_command += i[0] + "= ? AND "
-            ret_param.append(i[1])
-
-    sql_command = sql_command.strip().split(" ")
-    sql_command = " ".join(sql_command[:-1])
-
-    if parameters[3][1] in ["raw", "parsed"]:
-        if direction:
-            sql_command += " UNION SELECT * FROM opusfile WHERE source='"+so+"' AND target='"+ta+"' AND "        
-        else:
-            sql_command += " UNION SELECT * FROM opusfile WHERE ((source='"+so+"' AND target!='') OR (source!='' AND target='"+so+"')) AND "        
-        for i in parameters:
-            if i[0] == "preprocessing":
-                sql_command += "preprocessing='xml' AND "
-            elif i[1] != "#EMPTY#":
-                sql_command += i[0] + "= ? AND "
-                ret_param.append(i[1])
-        sql_command = sql_command.strip().split(" ")
-        sql_command = " ".join(sql_command[:-1])
-
-    return sql_command, tuple(ret_param)
-    
-def opusEntry(keys, values):
-    entry = dict(zip(tuple(keys), values))
-    #entry["url"] = "https://object.pouta.csc.fi/OPUS-"+entry["url"]
-    return entry
-
-def getLanguages(sou, cor):
-    if sou == "#EMPTY#" and cor == "#EMPTY#":
-        sql_command = "SELECT DISTINCT source FROM opusfile ORDER BY source"
-    elif sou != "#EMPTY#" and cor == "#EMPTY#":
-        sql_command = "SELECT source FROM opusfile WHERE target='"+sou+"' AND source!='"+sou+"' UNION SELECT target FROM opusfile WHERE source='"+sou+"' AND target!='' AND target!='"+sou+"';"
-    elif sou == "#EMPTY#" and cor != "#EMPTY#":
-        sql_command = "SELECT source FROM opusfile WHERE corpus='"+cor+"' UNION SELECT target FROM opusfile WHERE corpus='"+cor+"' AND target!='""';"
-    elif sou != "#EMPTY" and cor != "#EMPTY#":
-        sql_command = "SELECT source FROM opusfile WHERE corpus='"+cor+"' AND target='"+sou+"' AND source!='"+sou+"' UNION SELECT target FROM opusfile WHERE corpus='"+cor+"' AND source='"+sou+"' AND target!='' AND target!='"+sou+"';"
-    conn = opusapi_connection.connect()
-    query = conn.execute(sql_command)
-    return [i[0] for i in query.cursor]
-
-def getCorpora(sou, tar):
-    if sou == "#EMPTY#" and tar == "#EMPTY#":
-        sql_command = "SELECT DISTINCT corpus FROM opusfile ORDER BY corpus"
-    elif sou != "#EMPTY#" and tar == "#EMPTY#":
-        sql_command = "SELECT DISTINCT corpus FROM opusfile WHERE source='"+sou+"' ORDER BY corpus"
-    elif sou != "#EMPTY#" and tar != "#EMPTY#":
-        sql_command = "SELECT DISTINCT corpus FROM opusfile WHERE source='"+sou+"' AND target='"+tar+"' ORDER BY corpus"
-    conn = opusapi_connection.connect()
-    query = conn.execute(sql_command)
-    return [i[0] for i in query.cursor]
-
-def submitCommand(sql_command, params, direction):
-    conn = opusapi_connection.connect()
-    query = conn.execute(sql_command, params)
-
-    ret = [opusEntry(query.keys(), i) for i in query.cursor]
-    if direction:
-        found_corp = set()
-        for entry in ret:
-            if entry['source'] != '' and entry['target'] != '':
-                found_corp.add(entry['corpus'])
-        new_ret = []
-        for entry in ret:
-            if entry['corpus'] in found_corp:
-                new_ret.append(entry)
-        ret = new_ret
-
-    return ret
-
-def addRelatedMonoData(ret, parameters):
-    languages = set()
-    ret_param = []
-    for i in ret:
-        if i['source'] != parameters[0][1]:
-            languages.add(i['source'])
-        if i['target'] != '' and i['target'] != parameters[0][1]:
-            languages.add(i['target'])
-    lang_comm = " OR ".join(["source='"+lan+"'" for lan in languages])
-    sql_command = "SELECT * FROM opusfile WHERE ("+lang_comm+") AND target='' AND "        
-    for i in parameters[2:]:
-        if i[1] != "#EMPTY#":
-            sql_command += i[0] + "= ? AND "
-            ret_param.append(i[1])
-    sql_command = sql_command.strip().split(" ")
-    sql_command = " ".join(sql_command[:-1])
-
-    conn = opusapi_connection.connect()
-    query = conn.execute(sql_command, ret_param)
-
-    return ret + [opusEntry(query.keys(), i) for i in query.cursor]
+DB_FILE = os.environ["OPUSAPI_DB"]
 
 def clean_up_parameters(parameters):
-    ret_param = []
-    for item in parameters:
-        value = item[1].replace('"', '')
+    remove = []
+    valid_keys = ['corpus', 'id', 'latest', 'preprocessing', 'source', 'target', 'version', 'corpora', 'languages']
+    for key, value in parameters.items():
+        if key not in valid_keys:
+            remove.append(key)
+            continue
+        value = value.replace('"', '')
         value = value.replace('\'', '')
-        ret_param.append((item[0], value))
-    return ret_param
-   
+        parameters[key] = value
+    for key in remove:
+        del parameters[key]
+    return parameters
+
+def run_query(sql_command):
+    conn = sqlite3.connect(DB_FILE)
+    query = conn.execute(sql_command)
+    value_list = query.fetchall()
+    keys = [i[0] for i in query.description]
+    conn.close()
+    return keys, value_list
+
+def run_default_query(parameters, suffix=''):
+    sql_command = 'SELECT * FROM opusfile WHERE '+' AND '.join([f'{k} = "{v}"' for k, v in parameters.items()]) + suffix
+    keys, value_list = run_query(sql_command)
+    ret = [{k: v for k, v in zip(keys,values)} for values in value_list]
+    return ret
+
+def run_corpora_query(parameters):
+    sql_command = 'SELECT DISTINCT corpus FROM opusfile'
+    if len(parameters) > 0:
+        sql_command = sql_command+' WHERE '+' AND '.join([f'{k} = "{v}"' for k, v in parameters.items()])
+    _, value_list = run_query(sql_command)
+    values = [v[0] for v in value_list]
+    return values
+
+def run_languages_query(parameters):
+    sql_command = 'SELECT DISTINCT source FROM opusfile '
+    if len(parameters) > 0:
+        source = parameters.get('source')
+        if source:
+            sql_command = 'SELECT DISTINCT target FROM opusfile where '+' AND '.join([f'{k} = "{v}"' for k, v in parameters.items()]) + f' AND target != "{source}" AND target != "" UNION SELECT DISTINCT source FROM opusfile '
+            parameters['target'] = parameters['source']
+            del parameters['source']
+        sql_command = sql_command + 'WHERE '
+    sql_command = sql_command + ' AND '.join([f'{k} = "{v}"' for k, v in parameters.items()])
+    _, value_list = run_query(sql_command)
+    values = [v[0] for v in value_list]
+    return values
+
 @app.route('/')
 def opusapi():
-    source = request.args.get('source', '#EMPTY#', type=str)
-    target = request.args.get('target', '#EMPTY#', type=str)
-    corpus = request.args.get('corpus', '#EMPTY#', type=str)
-    preprocessing = request.args.get('preprocessing', '#EMPTY#', type=str)
-    version = request.args.get('version', '#EMPTY#', type=str)
-    languages = request.args.get('languages', False, type=bool)
-    corpora = request.args.get('corpora', False, type=bool)
-
-    sou_tar = [source, target]
-    sou_tar.sort()
-
-    direction = True
-    if "#EMPTY#" in sou_tar or "" in sou_tar:
-        sou_tar.sort(reverse=True)
-        direction = False
-
-    latest = "#EMPTY#"
-    if version == "latest":
-        version = "#EMPTY#"
-        latest = "True"
-    
-    parameters = [("source", sou_tar[0]), ("target", sou_tar[1]), ("corpus", corpus),
-                  ("preprocessing", preprocessing), ("version", version), 
-                  ("latest", latest)]
-
+    parameters = request.args.copy()
     parameters = clean_up_parameters(parameters)
 
-    sql_command, params = make_sql_command(parameters.copy(), direction)
+    source = parameters.get('source')
+    target = parameters.get('target')
 
-    if languages:
-        return jsonify(languages=getLanguages(parameters[0][1], parameters[2][1]))
-    if corpora:
-        return jsonify(corpora=getCorpora(parameters[0][1], parameters[1][1]))
-    if params == ():
+    if source and target:
+        sou_tar = sorted([source, target])
+        parameters['source'] = sou_tar[0]
+        parameters['target'] = sou_tar[1]
+
+    if len(parameters) == 0:
         #baseurl = 'http://opus.nlpl.eu/opusapi/'
         baseurl='http://127.0.0.1:5000/'
         return render_template('opusapi.html', baseurl=baseurl)
 
-    ret = submitCommand(sql_command, params, direction)
+    if 'corpora' in parameters.keys():
+        del parameters['corpora']
+        ret = run_corpora_query(parameters)
+        return jsonify(corpora=ret)
 
-    if len(ret)>0 and not direction and preprocessing in ["xml", "raw", "parsed", "#EMPTY#"]:
-        ret = addRelatedMonoData(ret, parameters)
+    if 'languages' in parameters.keys():
+        del parameters['languages']
+        ret = run_languages_query(parameters)
+        return jsonify(languages=ret)
+
+    version = parameters.get('version')
+    if version and version == 'latest':
+        parameters['latest'] = 'True'
+        del parameters['version']
+
+    ret = []
+
+    preprocessing = parameters.get('preprocessing')
+
+    if preprocessing in ['xml', 'raw', 'parsed']:
+        a_parameters = parameters.copy()
+        a_parameters['preprocessing'] = 'xml'
+        ret = ret + run_default_query(a_parameters, suffix=' AND target != ""')
+        languages = set()
+        for item in ret:
+            languages.add(item['source'])
+            languages.add(item['target'])
+
+        parameters['target'] = ''
+        for language in languages:
+            parameters['source'] = language
+            ret = ret + run_default_query(parameters)
+    else:
+        ret = run_default_query(parameters)
 
     return jsonify(corpora=ret)

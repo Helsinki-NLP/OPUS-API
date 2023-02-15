788e0cca446643f26d66f2d0c777eebdbbe1c473
miau1 <aulamo.mikko@gmail.com>
2023-02-15 10:30:35 +0200

    more testing

--- tests/test_db_operations.py
+++ tests/test_db_operations.py
@@ -17,6 +17,7 @@ class TestDbOperations(unittest.TestCase):
     def test_get_all_versions_bilingual(self):
         params = {'source': 'en', 'target': 'fi', 'corpus': 'OpenSubtitles', 'preprocessing': 'xml'}
         ret = run_default_query(params)
+        self.assertEqual(len(ret), 8)
         for i in ret:
             self.assertTrue(i['id'] in [112278, 112934, 114221, 115588, 123919, 123920, 136272, 136273])
 
@@ -28,6 +29,7 @@ class TestDbOperations(unittest.TestCase):
     def test_get_all_preprocessings_for_latest_bilingual(self):
         params = {'source': 'en', 'target': 'fi', 'corpus': 'OpenSubtitles', 'latest': 'True'}
         ret = run_default_query(params)
+        self.assertEqual(len(ret), 7)
         for i in ret:
             self.assertTrue(i['id'] in [126145, 128126, 130626, 130627, 133658, 136272, 136273])
 
@@ -44,6 +46,7 @@ class TestDbOperations(unittest.TestCase):
     def test_get_all_versions_monolingual(self):
         params = {'source': 'en', 'target': '', 'corpus': 'OpenSubtitles', 'preprocessing': 'xml'}
         ret = run_default_query(params)
+        self.assertEqual(len(ret), 6)
         for i in ret:
             self.assertTrue(i['id'] in [112299, 112972, 114258, 115629, 123968, 136362])
 
@@ -55,6 +58,7 @@ class TestDbOperations(unittest.TestCase):
     def test_get_all_preprocessings_for_latest_monolingual(self):
         params = {'source': 'en', 'target': '', 'corpus': 'OpenSubtitles', 'latest': 'True'}
         ret = run_default_query(params)
+        self.assertEqual(len(ret), 6)
         for i in ret:
             self.assertTrue(i['id'] in [127362, 127435, 127436, 129380, 129423, 136362])
 
@@ -101,12 +105,14 @@ class TestDbOperations(unittest.TestCase):
     def test_corpus_xml(self):
         params = {'source': 'en', 'target': 'fi', 'corpus': 'OpenSubtitles', 'preprocessing': 'xml', 'latest': 'True'}
         ret = get_corpora(params)
+        self.assertEqual(len(ret), 4)
         for i in ret:
             self.assertTrue(i['id'] in [136272, 136273, 136943, 136362])
 
     def test_corpus_raw(self):
         params = {'source': 'en', 'target': 'fi', 'corpus': 'OpenSubtitles', 'preprocessing': 'raw', 'latest': 'True'}
         ret = get_corpora(params)
+        self.assertEqual(len(ret), 4)
         for i in ret:
             self.assertTrue(i['id'] in [136272, 136273, 129423, 129429])
 
@@ -118,5 +124,19 @@ class TestDbOperations(unittest.TestCase):
     def test_corpus_xml_no_target(self):
         params = {'source': 'en', 'corpus': 'RF', 'preprocessing': 'xml', 'latest': 'True'}
         ret = get_corpora(params)
+        self.assertEqual(len(ret), 7)
         for i in ret:
             self.assertTrue(i['id'] in [140747, 140748, 140749, 140756, 140755, 140753, 140750])
+
+    def test_corpus_xml_unalphabetical(self):
+        params = {'source': 'fi', 'target': 'en', 'corpus': 'OpenSubtitles', 'preprocessing': 'xml', 'latest': 'True'}
+        ret = get_corpora(params)
+        for i in ret:
+            self.assertTrue(i['id'] in [136272, 136273, 136943, 136362])
+
+    def test_corpus_xml_no_langs(self):
+        params = {'corpus': 'RF', 'preprocessing': 'xml', 'latest': 'True'}
+        ret = get_corpora(params)
+        self.assertEqual(len(ret), 15)
+        for i in ret:
+            self.assertTrue(i['id'] in [140742, 140743, 140744, 140745, 140747, 140748, 140749, 140751, 140752, 140754, 140755, 140756, 140753, 140750, 140746])

1adce272f8b96873f6040a79a898f89ec986c9ff
miau1 <aulamo.mikko@gmail.com>
2023-04-14 14:19:51 +0300

    use opustools for db_operations

--- opusapi.py
+++ opusapi.py
@@ -1,13 +1,16 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
-from db_operations import clean_up_parameters, run_corpora_query, run_languages_query, get_corpora
+from opustools import DbOperations
 
 app = Flask(__name__)
 
 @app.route('/')
 def opusapi():
+    dbo = DbOperations(db_file=os.environ['OPUSAPI_DB'])
+
     parameters = request.args.copy()
-    parameters = clean_up_parameters(parameters)
+    parameters = dbo.clean_up_parameters(parameters)
 
     if len(parameters) == 0:
         #baseurl = 'http://opus.nlpl.eu/opusapi/'
@@ -15,9 +18,9 @@ def opusapi():
         return render_template('opusapi.html', baseurl=baseurl)
 
     if 'corpora' in parameters.keys():
-        return jsonify(corpora=run_corpora_query(parameters))
+        return jsonify(corpora=dbo.run_corpora_query(parameters))
 
     if 'languages' in parameters.keys():
-        return jsonify(languages=run_languages_query(parameters))
+        return jsonify(languages=dbo.run_languages_query(parameters))
 
-    return jsonify(corpora=get_corpora(parameters))
+    return jsonify(corpora=dbo.get_corpora(parameters))
